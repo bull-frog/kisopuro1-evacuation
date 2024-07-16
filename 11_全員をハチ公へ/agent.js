e5da82d9dd393d462ac921107098abe86ff7b700
bull-frog <bullfrog0603@gmail.com>
2024-07-16 17:27:49 +0900

    0716-1727

---
+++ 11_全員をハチ公へ/agent.js
@@ -0,0 +1,184 @@
+/**
+ * シミュレーションの時間間隔（秒）
+ */
+const timeInterval = 10;
+
+/**
+ * エージェントのクラス
+ * エージェントは、渋谷の道を歩く人間を表す
+ * エージェントは、年齢、現在いる道のID、現在いる道における位置、最終目的地のID、渋谷の道に対する知識度を持つ
+ */
+exports.Agent = class Agent {
+
+	/**
+	 * 属性と初期位置を指定し、新しいエージェントを生成する
+	 * @param {Number} id エージェントのID
+	 * @param {Number} age 年齢に応じてエージェントの移動距離を変えることを考える
+	 * @param {Number} initalStreetNumber 初期にいる道のID
+	 * @param {Number} initialPositionOnStreet 初期の道における位置（小さい番号側が0~大きい番号側が1の範囲で指定）
+	 * @param {Number} finalDestination 最終目的地のID、決まっていない場合は-1
+	 * @param {Number} familiarityWithShibuya 渋谷の道をよく知っているかどうか（数字で指定、とりあえず1-2）（将来的にはこれに応じて使用するルートを変えることも考える）
+	 * @param {Array<Array<Number>>} routes ダイクストラ法で得られた経路情報
+	 * @param {Array<{id: number, startNodeId: number, endNodeId: number, distance: number, width: number}>} linksWithDistances リンクの情報 (id, startNodeId, endNodeId, length, width)
+	 */
+	constructor(id, age, initalStreetNumber, initialPositionOnStreet, finalDestination, familiarityWithShibuya, routes, linksWithDistances) {
+		this.id = id;
+		this.age = age;
+		this.currentStreetNumber = initalStreetNumber;
+		this.currentPositionOnStreet = initialPositionOnStreet;
+		this.finalDestination = finalDestination;
+		this.familiarityWithShibuya = familiarityWithShibuya;
+		this.walkingDistancePerHour = 4000; // 通常の歩行速度（m/h）
+		this.isStacked = false; // 前が詰まっていて進めない場合はtrueにする
+
+		/**
+		 * エージェントが交差点にいるときは、その交差点のID。いないときは-1
+		 * @type {Number}
+		 */
+		this.currentNodeNumber = -1;
+
+		// エージェントは道のどこかに置かれるので、セットアップする。
+		
+		// 今いる道の両側の交差点のIDを取得
+		/**
+		 * @type {{id: number, startNodeId: number, endNodeId: number, distance: number}}
+		 */
+		const currentStreet = linksWithDistances[initalStreetNumber];
+		const upperNode = currentStreet.startNodeId;
+		const lowerNode = currentStreet.endNodeId;
+
+		// 目的地に向かうため、最初はどちらの交差点に向かうのかを決める。
+		if ((initialPositionOnStreet < 0.5 && routes[this.finalDestination][upperNode] == lowerNode) || (initialPositionOnStreet >= 0.5 && routes[this.finalDestination][lowerNode] != upperNode)) {
+			this.nextNodeNumber = lowerNode;
+			this.walkDirection = 1;
+		} else {
+			this.nextNodeNumber = upperNode;
+			this.walkDirection = -1;
+		}
+	}
+
+
+	/**
+	 * タイムステップごとに行う処理
+	 */
+	timestep(routes, linksWithDistances, linksFromNodes, peopleMovingStatusInStreets, populationDensityInStreets, peopleMovingStatusInNodes, nodeIsStacked) {
+
+		// エージェントが目的地に到着した場合
+		if (this.currentNodeNumber === this.finalDestination) {
+			return;
+		}
+
+		// エージェントが交差点にいる場合
+		if (this.currentNodeNumber != -1) {
+			
+			// 次に向かう交差点を決定
+			this.nextNodeNumber = routes[this.finalDestination][this.currentNodeNumber];
+			let nextLink = linksFromNodes[this.currentNodeNumber].find(link => link.destination === this.nextNodeNumber);
+			if (!nextLink) {
+				console.log(`Agent${this.id} cannot find the next link from node${this.currentNodeNumber} to node${this.nextNodeNumber}`);
+			}
+
+			if (populationDensityInStreets[nextLink.linkId] <= 6) {
+				// 次に向かう道の人口密度が6人/m^2以下の場合は、道に移動。
+				this.isStacked = false;
+				this.currentStreetNumber = nextLink.linkId;
+				this.currentPositionOnStreet = nextLink.up ? 1 : 0;
+				this.walkDirection = nextLink.up ? -1 : 1;
+				this.currentNodeNumber = -1;
+			} else {
+				// 次に向かう道の人口密度が6人/m^2を超える場合は、交差点に留まる。
+				this.isStacked = true;
+			}
+
+		}
+		// エージェントが道にいる場合
+		else if (this.currentStreetNumber != -1) {
+
+			// linksWithDistancesから、今いる道の長さを取得する
+			const currentStreetLength = linksWithDistances[this.currentStreetNumber].distance;
+
+			/*
+			 * 道の密度によって、歩く速度を変える。密度と速度の関係は、
+			 * https://www.bousai.go.jp/kaigirep/chuobou/senmon/shutohinan/10/
+			 * で与えられる通りとする。
+			 * 双方向流を考慮するには至っていない
+			 */
+			// 道にいる場合、今いる道の密度を取得
+			if (!this.currentStreetNumber !== -1) {
+				const currentStreetPopulationDensity = populationDensityInStreets[this.currentStreetNumber];
+				if (currentStreetPopulationDensity < 1.5) {
+					this.walkingDistancePerHour = 4000;
+				} else if (currentStreetPopulationDensity < 6) {
+					this.walkingDistancePerHour = 5200 - 800 * currentStreetPopulationDensity;
+				} else if (currentStreetPopulationDensity < 1.5) {
+					this.walkingDistancePerHour = 2400 / currentStreetPopulationDensity;
+				} else {
+					this.walkingDistancePerHour = 1000;
+				}
+			}
+
+			// timeInterval(s)ごとに、currentPositionOnStreetを増減する
+			this.currentPositionOnStreet += (this.walkingDistancePerHour / 3600 * timeInterval / currentStreetLength * this.walkDirection);
+
+			// currentPositionOnStreetが(0,1)を出たら、交差点に移動したものと判断する。ただし、移動先の交差点にisStackedがtrueであるエージェントが存在する場合は、移動しない。
+			if ((this.currentPositionOnStreet <= 0 || this.currentPositionOnStreet >= 1) && !nodeIsStacked[this.nextNodeNumber]) {
+				this.isStacked = false;
+				this.currentNodeNumber = this.nextNodeNumber;
+				this.nextNodeNumber = -1;
+				this.currentStreetNumber = -1;
+			} else {
+				this.isStacked = true;
+				this.walkingDistancePerHour = 0;
+			}
+
+		}
+	}
+
+	/**
+	 * エージェントの情報を返す
+	 */
+	getStatus() {
+		return {
+			id: this.id,
+			age: this.age,
+			currentStreetNumber: this.currentStreetNumber,
+			currentPositionOnStreet: this.currentPositionOnStreet,
+			currentNodeNumber: this.currentNodeNumber,
+			nextNodeNumber: this.nextNodeNumber,
+			finalDestination: this.finalDestination,
+			familiarityWithShibuya: this.familiarityWithShibuya,
+			walkingDistancePerHour: this.walkingDistancePerHour,
+			isStacked: this.isStacked,
+			walkDirection: this.walkDirection
+		};
+	}
+	
+
+};
+
+
+/**
+ * 所与の分布に沿ってエージェントを生成する
+ * @param {Array<Agent>} agents エージェントを格納する配列
+ * @param {Array<{id: number, startNodeId: number, endNodeId: number, distance: number, width: number}>} linksWithDistances リンクの情報 (id, startNodeId, endNodeId, length, width)
+ * @param {Array<Array<Number>>} routes ダイクストラ法で得られた経路情報
+ */
+exports.generateAgents = function(linksWithDistances, routes) {
+
+	// agentsを初期化
+	let agents = [];
+
+	// とりあえず、道の長さに従って等密度で分布させる。目的地も仮に、全員がハチ公前（ID: 187）に向かうものとする。
+	const totalDistanceOfLinks = linksWithDistances.reduce((acc, link) => acc + link.distance, 0);
+	const totalPopulation = 50000;
+
+	linksWithDistances.forEach((link, index) => {
+		const numberOfAgents = Math.round(totalPopulation * link.distance / totalDistanceOfLinks);
+		for (let i = 0; i < numberOfAgents; i++) {
+			agents.push(new exports.Agent(agents.length, 20, index, Math.random(), 187, Math.round(Math.random() + 1), routes, linksWithDistances));
+		}
+	});
+
+	return agents
+
+}
---
+++ 11_全員をハチ公へ/visualize-hachiko-population.js
@@ -0,0 +1,107 @@
+const { loadCSVWithSpecifiedFilename } = require("./loadCSV");
+
+const calculateDistance = require("./links").calculateDistance;
+const createLinksFromNodes = require("./links").createLinksFromNodes;
+const loadCSV = require("./loadCSV").loadCSV;
+const dijkstra = require("./dijkstra").dijkstra;
+const Agent = require("./agent").Agent;
+const generateAgents = require("./agent").generateAgents;
+const Street = require("./street");
+const fs = require("fs");
+
+const timeInterval = 10; // シミュレーションの時間間隔 (s)
+
+
+// ノード・リンクのデータを読み込み、前処理を行う
+
+/**
+ * @typedef {[id: number, startId: number, endId: number, width: number, wkt: string]} Link
+ * @type {Array<Link>}
+ */
+const links = loadCSV("links-with-width-wkt.csv", 0);
+
+/**
+ * @typedef {[id: number, lon: number, lat: number]} Node
+ * @type {Array<Node>}
+ */
+const nodes = loadCSV("nodes.csv", 0);
+
+/**
+ * リンクの距離情報を持つオブジェクトの配列
+ * @type {Array<{id: number, startNodeId: number, endNodeId: number, distance: number, width: number}>}
+ */
+const linksWithDistances = calculateDistance(nodes, links); // 距離を計算
+
+/**
+ * そのノードから出るリンクのID、行き先、方向を持ったオブジェクトの配列
+ * @type {Array<Array<{linkId: number, destination: number, up: boolean}>>}
+ */
+const linksFromNodes = createLinksFromNodes(nodes, links);
+
+/**
+ * 各始点から全ての点への経路上の次の点を格納した二次元配列。
+ * routes[目的地][現在地]で、現在地から目的地へ行く最短経路上の次の点が得られる。
+ * 目的地 = 現在地の場合には、-1が格納される。
+ * @type {Array<Array<number>>}
+ */
+const routes = [];
+for (let i = 0; i < nodes.length; i++) {
+	routes.push(dijkstra(linksWithDistances, nodes.length, i).nextPointOnRoute);
+}
+
+// エージェントの初期化
+let agents = generateAgents(linksWithDistances, routes);
+
+// 状態変数の定義
+let streetSituations = Street.updateAgentsInStreets(agents, linksWithDistances);
+let agentsInStreets = streetSituations.agentsInStreets;
+let populationDensityInStreets = streetSituations.populationDensityInStreets;
+let peopleMovingStatusInStreets = streetSituations.peopleMovingStatusInStreets;
+let nodeSituations = Street.updateAgentsInNodes(agents, nodes);
+let agentsInNodes = nodeSituations.agentsInNodes;
+let totalPopulationInNodes = nodeSituations.totalPopulationInNodes;
+let peopleMovingStatusInNodes = nodeSituations.peopleMovingStatusInNodes;
+let nodeIsStacked = nodeSituations.nodeIsStacked;
+
+
+
+// シミュレーションの記録を格納する配列などはここで初期化すべし
+let hachikoPopulation = [];
+
+
+// タイムステップの実行
+for (let t = 0; t < 200; t++) {
+
+	// 時刻を表示
+	console.log(`~~~ 避難開始から ${(t * timeInterval / 60).toFixed(1)} 分 ~~~`);
+
+	// エージェントを動かす
+	agents.forEach(agent => agent.timestep(routes, linksWithDistances, linksFromNodes, peopleMovingStatusInStreets, populationDensityInStreets, peopleMovingStatusInNodes, nodeIsStacked));
+
+	// 状態変数を更新
+	streetSituations = Street.updateAgentsInStreets(agents, linksWithDistances);
+	agentsInStreets = streetSituations.agentsInStreets;
+	populationDensityInStreets = streetSituations.populationDensityInStreets;
+	peopleMovingStatusInStreets = streetSituations.peopleMovingStatusInStreets;
+	nodeSituations = Street.updateAgentsInNodes(agents, nodes);
+	agentsInNodes = nodeSituations.agentsInNodes;
+	totalPopulationInNodes = nodeSituations.totalPopulationInNodes;
+	peopleMovingStatusInNodes = nodeSituations.peopleMovingStatusInNodes;
+	nodeIsStacked = nodeSituations.nodeIsStacked;
+
+	// いくつかのエージェントの情報を表示
+	// [0, 5000, 10000, 15000, 20000, 25000, 30000, 35000, 40000, 45000].forEach(i => {
+	// 	const agent = agents[i];
+	// 	console.log(`Agent${i} is at ${agent.currentStreetNumber != -1 ? "link" + agent.currentStreetNumber : "node" + agent.currentNodeNumber} and is ${agent.isStacked ? "stacked" : "moving to node" + agent.nextNodeNumber}`);
+	// });
+
+	// シミュレーションの記録を格納すべし
+	hachikoPopulation.push(agentsInNodes[187].length);
+	// console.log(`Population at Hachiko Square: ${agentsInNodes[187].length}`);
+
+}
+
+// シミュレーションの記録を出力する
+const csv = hachikoPopulation.map((value, index) => `${index * timeInterval},${value}`).join("\n");
+console.log(csv);
+fs.writeFileSync("hachikoPopulation.csv", "time,population\n" + csv);
